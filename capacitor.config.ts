395fad16a83c95c67515584415d33a4954364d4f
usriyatulll <102362593+usriyatulll@users.noreply.github.com>
2023-11-14 13:02:37 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ppmP10',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
